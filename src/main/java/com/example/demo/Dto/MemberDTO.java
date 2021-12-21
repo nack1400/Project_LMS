94f33c35d6663ad21fc6119e2798cc78d4900260
nack1400 <nack1400@naver.com>
2021-12-21 17:20:08 +0900

    template

---
+++ src/main/java/com/example/demo/Dto/MemberDTO.java
@@ -0,0 +1,13 @@
+package com.example.demo.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberDTO {
+    private String name;
+    private String id;
+    private String pwd;
+    private String email;
+}
