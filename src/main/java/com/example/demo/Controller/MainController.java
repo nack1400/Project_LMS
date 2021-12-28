0458df4f23f644fb560880275c8f653000fe921c
nack1400 <nack1400@naver.com>
2021-12-28 17:24:50 +0900

    Make Class

--- src/main/java/com/example/demo/Controller/MainController.java
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -3,6 +3,8 @@ package com.example.demo.Controller;
 import com.example.demo.Dto.LectureDTO;
 import com.example.demo.Dto.MemberDTO;
 import com.example.demo.Entity.Lecture;
+import com.example.demo.Repository.ClassRepository;
+import com.example.demo.Service.ClassService;
 import com.example.demo.Service.LectureService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -22,26 +24,25 @@ import java.util.List;
 public class MainController {
     @Autowired
     private final LectureService service;
+    @Autowired
+    private final ClassService serviceclass;
 
     @GetMapping("/main")
     public String mainpage(LectureDTO dto,HttpServletRequest req, Model model){
         HttpSession session = req.getSession();
 
-        List<Lecture> lecture = service.findByTeacherId(session.getAttribute("id").toString());
-        System.out.println("세션 아이디 : " + session.getAttribute("id").toString());
-        model.addAttribute("lecture", lecture);
-        System.out.println("확인 : "+lecture.toString());
+
         if("1".equals(session.getAttribute("auth").toString())){
+            List<Lecture> lecture = service.findByTeacherId(session.getAttribute("id").toString());
+            System.out.println("세션 아이디 : " + session.getAttribute("id").toString());
+            model.addAttribute("lecture", lecture);
+            System.out.println("확인 : "+lecture.toString());
             return "/view/teacher_main.html"; //강사
         }else{
             return "/view/student_main.html";//학생
         }
     }
 
-//    @GetMapping("/makelecture")
-//    public String makelecture(){
-//        return "/view/makeLecture.html";
-//    }
     @GetMapping("/lectureproc")
     public String lectureProc(){
         return "/view/makeLecture.html";
@@ -53,20 +54,14 @@ public class MainController {
         service.save(dto,req);
         return "redirect:/main";
     }
-//
-//    @PostMapping("/lectureproc")
-//    public String lectureproc(LectureDTO dto, HttpServletRequest req){
-//        System.out.println(dto.toString());
-//        service.save(dto,req);
-//        return "/main";
-//    }
 
-    @GetMapping("/lecturelist")
-    public String lectureList(LectureDTO dto,HttpServletRequest req, Model model){
-        List<Lecture> lecture = service.findByTeacherId(dto.getTeacherId());
-        model.addAttribute("lecture", lecture);
-        //여기까지
-        System.out.println(lecture);
-        return "redirect:/main";
+    @GetMapping("/registerlecture")
+    public String registerlecture(){
+        return "registerLecture.html";
     }
+
+//    @PostMapping("/registerclass")
+//    public String registerclass(LectureDTO dto, HttpServletRequest req){
+//        serviceclass.
+//    }
 }
---
+++ src/main/java/com/example/demo/Repository/ClassRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.Class;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClassRepository extends JpaRepository<Class, Long> {
+    List<Class> findByStudentId(String sid);
+}
